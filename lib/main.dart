feb1fa40b39049d76c338eb88fdd61689585a333
Anukul <anukul.030601@gmail.com>
2022-03-08 23:11:00 +0530

    base code

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,10 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TITLE",
-        style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
-        centerTitle: true,
-        backgroundColor: Colors.red,
         
       ),
       body:Container(
-        color: Colors.amber[300] ,
-        child:Text("I am the container"),
-        margin: EdgeInsets.fromLTRB(10, 20, 30, 40),
-        padding: EdgeInsets.fromLTRB(40, 30, 20, 10),
+       
         )
       );
   }
